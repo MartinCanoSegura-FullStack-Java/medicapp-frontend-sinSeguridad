e0cdd2e217840e220b8ea7d94662abc230598122
Martin Cano Segura <mecano_mcs@hotmail.com>
2022-09-28 19:47:28 -0500

    primer commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BuscarComponent } from './pages/buscar/buscar.component';
+import { ConsultaEspecialComponent } from './pages/consulta-especial/consulta-especial.component';
+import { ConsultaComponent } from './pages/consulta/consulta.component';
+import { WizardComponent } from './pages/consulta/wizard/wizard.component';
+import { EspecialidadEdicionComponent } from './pages/especialidad/especialidad-edicion/especialidad-edicion.component';
+import { EspecialidadComponent } from './pages/especialidad/especialidad.component';
+import { ExamenEdicionComponent } from './pages/examen/examen-edicion/examen-edicion.component';
+import { ExamenComponent } from './pages/examen/examen.component';
+import { MedicoComponent } from './pages/medico/medico.component';
+import { PacienteEdicionComponent } from './pages/paciente/paciente-edicion/paciente-edicion.component';
+import { PacienteComponent } from './pages/paciente/paciente.component';
+import { ReporteComponent } from './pages/reporte/reporte.component';
+
+const routes: Routes = [
+  {
+    path: 'paciente', component: PacienteComponent, children: [
+      { path: 'nuevo', component: PacienteEdicionComponent },
+      { path: 'edicion/:id', component: PacienteEdicionComponent }
+    ]
+  },
+  {
+    path: 'examen', component: ExamenComponent, children: [
+      { path: 'nuevo', component: ExamenEdicionComponent },
+      { path: 'edicion/:id', component: ExamenEdicionComponent }
+    ]
+  },
+  {
+    path: 'especialidad', component: EspecialidadComponent, children: [
+      { path: 'nuevo', component: EspecialidadEdicionComponent },
+      { path: 'edicion/:id', component: EspecialidadEdicionComponent }
+    ]
+  },
+  { path: 'medico', component: MedicoComponent },
+  { path: 'consulta', component: ConsultaComponent },
+  { path: 'consulta-wizard', component: WizardComponent },
+  { path: 'consulta-especial', component: ConsultaEspecialComponent },
+  { path: 'buscar', component: BuscarComponent },
+  { path: 'reporte', component: ReporteComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
